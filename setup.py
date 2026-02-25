f6b85ce9b632fc1dd7efba15d0a9f24c03c77f9a
Thor Astrup Pedersen <thorastrup@gmail.com>
2026-02-25 17:22:48 +0100

    feat: setup information

--- setup.py
+++ setup.py
@@ -3,6 +3,12 @@ from setuptools import setup, find_packages
 setup(
     name="tdmatrixbot",
     version="0.1.0",
+    author="TapDev",
+    author_email="tapdevdenmark@gmail.com",
+    description="Python module for making Matrix bots.",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/tapdevdk/tdmatrixbot",
     packages=find_packages(),
     install_requires=[
         "asyncio>=4.0.0,<5.0.0",
@@ -10,4 +16,6 @@ setup(
         "livekit>=1.1.2,<2.0.0",
         "markdown>=3.10.2,<4.0.0",
     ],
+    python_requires=">=3.12",
+    license="MIT License",
 )

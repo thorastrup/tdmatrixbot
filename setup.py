1079a44be94a47a5c379c6029d0d905dfcc889fd
Thor Astrup Pedersen <thorastrup@gmail.com>
2026-02-25 17:24:04 +0100

    feat: bumped version to 0.1.1

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="tdmatrixbot",
-    version="0.1.0",
+    version="0.1.1",
     author="TapDev",
     author_email="tapdevdenmark@gmail.com",
     description="Python module for making Matrix bots.",
